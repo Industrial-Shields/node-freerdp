590f9f3b9a3049d307eb0c55ee3d97ff7e7856f2
moatazomr <moataz.omr@gmail.com>
2020-01-18 01:06:47 +0300

    Update rdp-client.js

--- rdp-client.js
+++ rdp-client.js
@@ -1,1 +1,26 @@
 
+
+var freerdp = require('freerdp');
+
+var session = new freerdp.Session({
+  host: '51.140.125.102',
+  username: 'admin',
+  password: 'Pass@azure12',
+  domain: '', // optional
+  port: 3389, // optional
+  width: 1366, // optional
+  height: 768, // optional
+  certIgnore: true // optional
+});
+
+session.on('connect', function () {
+  console.log('connected');
+
+  var x = 10, y = 20; // if x / y are null, will not move mouse
+  session.sendPointerEvent(x, y, {
+    pressLeft: true // Other options are pressMiddle, pressRight, releaseLeft|Middle|Right
+  });
+
+  var code = 0x23; // letter 'H'
+  var isPressed = true;
+  session.sendKeyEventScancode(code, isPressed);
